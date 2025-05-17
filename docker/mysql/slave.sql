439392f21eab44bd2d5a966ddd995a891ccbd6d2
ravilushqa <ravilushqa@users.noreply.github.com>
2025-05-17 17:11:45 +0200

    Enhance MySQL replication setup and user management in Makefile and SQL scripts (#12)

--- docker/mysql/slave.sql
+++ docker/mysql/slave.sql
@@ -1,3 +1,14 @@
-RESET SLAVE;
-CHANGE MASTER TO MASTER_HOST='mysql_master', MASTER_USER='repl', MASTER_PASSWORD='slavepass';
-START SLAVE;
+-- Check if slave is already running
+SET @slave_running := (SELECT COUNT(*) FROM performance_schema.replication_connection_status);
+
+-- Only reset and restart slave if not already running
+SET @sql := IF(@slave_running > 0, 
+    'SELECT "Slave already running, skipping initialization"', 
+    CONCAT('RESET SLAVE; ',
+          'DO SLEEP(10); ', -- Increased delay for master to be ready
+          'CHANGE MASTER TO MASTER_HOST="mysql_master", MASTER_USER="repl", MASTER_PASSWORD="slavepass"; ',
+          'START SLAVE;'));
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
